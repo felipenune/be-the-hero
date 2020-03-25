e414c723a0276c249bcf22d8ac46907f62692e16
Felipe Nunes <felipenunesfob@gmail.com>
2020-03-25 11:57:44 -0300

    Register Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Login from './pages/login'
+
 import './global.css'
+
+import Routes from './routes'
 
 export default function App() {  
   return (
-    <Login />
+    <Routes />
   );
 }
--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {FiLogIn} from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 import heroesImg from '../../assets/heroes.png'
@@ -17,10 +18,10 @@ export default function Login(){
                     <input placeholder="Sua ID" />
                     <button type="submit" className="button">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color={"#e02041"} />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes" />
